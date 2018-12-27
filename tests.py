054a6fa97ed03c3003d090bb93f694c4885dc579
Johannes <seikowsky@googlemail.com>
2018-12-27 01:44:24 +0000

    ...

--- tests.py
+++ tests.py
@@ -1,12 +1,10 @@
 # selectively testing functionalities
 import unittest
-from utils import get_date
-from email_rem import save_reminder, send_relevant_reminders
+from utils import get_date, save_reminder, send_todays_reminders 
 from configs import path_to_storage
 
 
 class TestClass(unittest.TestCase):
-
 	def test_get_date(self):
 		date = get_date()
 		# check length and composition of date string
@@ -16,14 +14,8 @@ class TestClass(unittest.TestCase):
 		for each in parts_of_date:
 			self.assertTrue(each.isdigit() and len(each) == 2)
 
-	# def test_save_reminder(self):
-	# 	test_rem_msg = "this is the test reminder"
-	# 	test_rem_date = "12.12.2012"
-	# 	save_reminder(test_rem_msg, test_rem_date, path_to_storage)
-
-
-
-
+		# to be added -
+		# test for save_reminder and send_todays_reminders
 
 if __name__ == "__main__":
 	unittest.main(exit=False)
